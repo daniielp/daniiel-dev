0ad27329ab3e1c4b52cae006a32aac64897f45dc
shorcy <59348193+shorcy@users.noreply.github.com>
2021-06-11 13:16:21 +0200

    Mobile responsive

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import { useState, useEffect } from 'react'
-import { Container, makeStyles, Typography } from '@material-ui/core'
-import { supabase } from '../lib/initSupabase';
-import BodyText from '../components/BodyText';
+import { makeStyles } from '@material-ui/core'
 import Slider from '../components/Slider';
 
 const useStyles = makeStyles((theme) => ({
@@ -23,29 +20,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Home() {
-  const [infoBox, SetInfoBox] = useState([])
-
-  useEffect(() => {
-    fetchInfoBox()
-  }, [])
-
-  console.log(infoBox)
-
-  const fetchInfoBox = async () => {
-    let { data: infoBox, error } = await supabase.from('InfoBox').select('*').order("Sorting")
-    if (error) return console.error('Error', error)
-    SetInfoBox(infoBox)
-  }
 
   const classes = useStyles();
   return (
     <main className={classes.root}>
       <Slider />
-      <section className={classes.fullHeight}>
-        {/* <Typography variant="h3" component="h3" color="textSecondary">- {infoBox[0]?.Subheadline}</Typography>
-        <Typography variant="h2" component="h2" color="">{infoBox[0]?.Headline}</Typography>
-        <BodyText>{infoBox[0]?.BodyText}</BodyText> */}
-      </section>
+      {/* <section className={classes.fullHeight}>
+      </section> */}
     </main>
   )
 }
