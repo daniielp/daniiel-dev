aece5141064e9c8d1dd0fe45e2556b6e0bae3721
shorcy <59348193+shorcy@users.noreply.github.com>
2021-10-18 08:40:17 +0200

    Update Testimonial.tsx

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,13 +1,19 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { Container, makeStyles} from '@material-ui/core';
 import { Theme } from '@material-ui/core/styles'
 import TextBundle from './TextBundle';
+import Image from 'next/image'
+import BodyText from './BodyText';
 
 const useStyles = makeStyles((theme: Theme) => ({
     testimonial: {
         backgroundColor: theme.palette.secondary.dark,
         paddingTop: theme.spacing(10),
         paddingBottom: theme.spacing(10)
+    },
+    button: {
+        color: theme.palette.primary.main,
+        fontWeight: 'bold'
     }
 }))
 
@@ -20,6 +26,17 @@ const Testimonial = () => {
                 <Grid container spacing={2}>
                     <Grid item md={6}>
                         <TextBundle title="Hvad siger folk?" subtitle="Anmeldelser" text="Jeg har haft muligheden for at arbejde med nogle helt fantastiske mennesker, og tænkte jeg lige ville dele hvad de har sagt om mig." />
+                        <Button className={classes.button}>Se mere</Button>
+                    </Grid>
+                    <Grid item md={6}>
+                        <Grid container>
+                            <Grid item md={4}>
+                                {/* <Image alt="Author" /> */}
+                            </Grid>
+                            <Grid item md={8}>
+                                <BodyText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed. Eu turpis egestas pretium aenean pharetra. Porttitor leo a diam sollicitudin tempor id eu. Consequat interdum varius sit amet mattis vulputate enim nulla. </BodyText>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container>
