957146b7006a20a9d15ca8e450b83982398ce6e5
shorcy <59348193+shorcy@users.noreply.github.com>
2021-10-25 08:09:55 +0200

    Added footer

---
+++ Layouts/MainLayout/Footer.js
@@ -0,0 +1,19 @@
+import { Grid } from "@material-ui/core"
+import TextBundle from "../../components/TextBundle"
+
+const Footer = () => {
+    return (
+        <>
+        <Grid container>
+            <Grid item md={6}>
+                <TextBundle />
+            </Grid>
+        </Grid>
+        <footer>
+
+        </footer>
+        </>
+    )
+}
+
+export default Footer
--- Layouts/MainLayout/index.js
+++ Layouts/MainLayout/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 function index(props) {
     return (
         <React.Fragment>
             <Header />
             {props.children}
+            <Footer />
         </React.Fragment>
     )
 }
