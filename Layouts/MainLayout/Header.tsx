63852dea4636d5bfb68cf6fbdb1ea00c1673356c
shorcy <59348193+shorcy@users.noreply.github.com>
2021-11-10 14:03:49 +0100

    Update menu with drawer

--- Layouts/MainLayout/Header.tsx
+++ Layouts/MainLayout/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { AppBar, Avatar, Container, Toolbar, Typography, Button, Theme } from '@mui/material';
+import { AppBar, Container, Toolbar, Typography, Button, Theme, Hidden, IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import Image from 'next/image';
 import Logo from '../../assets/images/logo.svg'
+import Menu from '../../assets/Icons/menu.svg'
+import Close from '../../assets/Icons/close.svg'
 import Link from 'next/link'
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme: Theme) => ({
   background: {
@@ -22,12 +25,29 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   toolbar: {
     padding: 0,
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  drawer: {
+    width: '100%',
+    backgroundColor: theme.palette.secondary.dark,
+    backgroundImage: 'none',
+  },
+  closeMenu: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    borderBottom: '1px solid ' + theme.palette.secondary.light
   }
 }));
 
 const Header = (props: any) => {
   const classes = useStyles();
   const { pages } = props;
+  const [drawer, setDrawer] = useState(false);
+
+  console.log(drawer)
 
   return (
     <AppBar position="sticky" className={classes.background}>
@@ -42,9 +62,43 @@ const Header = (props: any) => {
             >
               <Typography variant="h6" component="h1" color="textPrimary">
                 Daniel Pedersen
-          </Typography>
+              </Typography>
             </Button>
           </Link>
+          <Hidden smDown>
+            <nav>
+              <Button variant="text">Om mig</Button>
+            </nav>
+          </Hidden>
+          <Hidden mdUp>
+            <IconButton color="inherit" aria-label="Main Menu" onClick={() => setDrawer(prevState => !prevState)}><Image src={Menu} width={35} height={35} alt="menu" /></IconButton>
+            <Drawer anchor="left" open={drawer} onClose={() => setDrawer(prevState => !prevState)} PaperProps={{ className: classes.drawer }}>
+              <Container maxWidth="lg">
+                <div className={classes.closeMenu}>
+                  <Link href="/">
+                    <Button
+                      variant="contained"
+                      startIcon={<Image src={Logo} width={45} height={45} />}
+                      className={classes.clicableButton}
+                      disableRipple
+                    >
+                      <Typography variant="h6" component="h1" color="textPrimary">
+                        Daniel Pedersen
+                      </Typography>
+                    </Button>
+                  </Link>
+                  <IconButton color="inherit" aria-label="Close menu" onClick={() => setDrawer(prevState => !prevState)}><Image src={Close} width={35} height={35} alt="menu" /></IconButton>
+                </div>
+
+                <List component="nav" aria-labelledby="menu list">
+                  <ListItem>
+                    <ListItemText primary="Om mig" />
+                  </ListItem>
+                </List>
+
+              </Container>
+            </Drawer>
+          </Hidden>
         </Toolbar>
       </Container>
     </AppBar>
