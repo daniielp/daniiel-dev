edf76ea5b1a341e955e485bb68090c32ac072d02
shorcy <59348193+shorcy@users.noreply.github.com>
2021-11-11 10:53:47 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
+import ServerStyleSheets from "@mui/styles/ServerStyleSheets";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+    const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  })
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
 
-  const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ]
-  }
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
 
   render() {
     return (
       <Html lang="da">
-        <Head >
+        <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#f5d027" />
