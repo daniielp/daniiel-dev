dbc844f31d9cff4982ae40275fe155c6cfe49866
shorcy <59348193+shorcy@users.noreply.github.com>
2022-01-24 16:55:25 +0100

    Fix mobile menu not closing

--- Layouts/MainLayout/Header.tsx
+++ Layouts/MainLayout/Header.tsx
@@ -24,17 +24,17 @@ const Header = () => {
   const [drawer, setDrawer] = useState(false);
 
   return (
-    <AppBar position="sticky" sx={{bgcolor: 'secondary.main'}}>
-       <Button href="#main-content" component="a" sx={{position: 'absolute', transform: 'translateY(-120%)' ,'&:focus': {transform: 'translateY(0)'}}}>
-          Skip Navigation
-        </Button>
+    <AppBar position="sticky" sx={{ bgcolor: 'secondary.main' }}>
+      <Button href="#main-content" component="a" sx={{ position: 'absolute', transform: 'translateY(-120%)', '&:focus': { transform: 'translateY(0)' } }}>
+        Skip Navigation
+      </Button>
       <Container maxWidth="lg">
-        <Toolbar sx={{p: [0,0,0,0,0], display: 'flex', justifyContent: 'space-between'}}>
+        <Toolbar sx={{ p: [0, 0, 0, 0, 0], display: 'flex', justifyContent: 'space-between' }}>
           <Link href="/">
             <Button
               variant="contained"
               startIcon={<Image src={Logo} width={45} height={45} />}
-              sx={{bgcolor: 'transparent', boxShadow: 'none', m: 0, p: 0, '&:hover': {boxShadow: 'none', background: 'transparent'}}}
+              sx={{ bgcolor: 'transparent', boxShadow: 'none', m: 0, p: 0, '&:hover': { boxShadow: 'none', background: 'transparent' } }}
               disableRipple
             >
               <Typography variant="h6" component="h1" color="textPrimary">
@@ -51,15 +51,16 @@ const Header = () => {
           </Hidden>
           <Hidden mdUp>
             <IconButton color="inherit" aria-label="Main Menu" onClick={() => setDrawer(prevState => !prevState)}><Image src={Menu} width={35} height={35} alt="menu" /></IconButton>
-            <Drawer anchor="left" open={drawer} onClose={() => setDrawer(prevState => !prevState)} PaperProps={{style: {width: '100%', backgroundColor: '#272c3a', backgroundImage: 'none'}}}>
+            <Drawer anchor="left" open={drawer} onClose={() => setDrawer(prevState => !prevState)} PaperProps={{ style: { width: '100%', backgroundColor: '#272c3a', backgroundImage: 'none' } }}>
               <Container maxWidth="lg">
-                <Box sx={{display: 'flex', justifyContent: 'space-between', py: 1,borderBottom: 1, borderColor: 'secondary.light'}}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', py: 1, borderBottom: 1, borderColor: 'secondary.light' }}>
                   <Link href="/">
                     <Button
                       variant="contained"
                       startIcon={<Image src={Logo} width={45} height={45} />}
-                      sx={{bgcolor: 'transparent', boxShadow: 'none', m: 0, p: 0, '&:hover': {boxShadow: 'none', background: 'transparent'}}}
+                      sx={{ bgcolor: 'transparent', boxShadow: 'none', m: 0, p: 0, '&:hover': { boxShadow: 'none', background: 'transparent' } }}
                       disableRipple
+                      onClick={() => setDrawer(prevState => !prevState)}
                     >
                       <Typography variant="h6" component="h1" color="textPrimary">
                         Daniel Pedersen
@@ -70,9 +71,11 @@ const Header = () => {
                 </Box>
 
                 <List component="nav" aria-labelledby="menu list">
-                  <ListItem>
-                    <ListItemText primary="Om mig" />
-                  </ListItem>
+                  <Link href="/om-mig">
+                    <ListItem onClick={() => setDrawer(prevState => !prevState)}>
+                      <ListItemText primary="Om mig" />
+                    </ListItem>
+                  </Link>
                 </List>
 
               </Container>
