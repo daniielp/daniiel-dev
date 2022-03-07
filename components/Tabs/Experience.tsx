b2264817d647da42c4798cd20efe694fe70f56ab
shorcy <59348193+shorcy@users.noreply.github.com>
2022-03-07 12:15:25 +0100

    Fix CLS on Experience

--- components/Tabs/Experience.tsx
+++ components/Tabs/Experience.tsx
@@ -25,27 +25,27 @@ const Hero = () => {
                     </Grid>
                     <Grid item md={6} sx={{display: 'flex', flexDirection: ['column-reverse', 'column-reverse', 'column'], justifyContent: "center",}} >
                         <TabContext value={tabIndex.toString()}>
-                            <TabPanel value="0" sx={{padding: 0}}>
+                            <TabPanel value="0" sx={{padding: 0, flexGrow: 1}}>
                                 <Grid container spacing={0}>
                                     <Grid item md={6} sx={{bgcolor: 'secondary.dark', p:2}}>
                                         <Typography variant="h6" component="h4">Fullstack udvikler</Typography>
                                         <Typography variant="subtitle1" component="h4">Admill.dk</Typography>
                                         <BodyText>Hos admill var jeg en del af et større team, som havde til ansvar at holde styr på deres I alt 20+ sider.</BodyText>
                                     </Grid>
-                                    <Grid item md={6} sx={{p: 2, borderTop: 2, borderRight: 2, borderBottom: 2, borderColor: 'primary.main', borderLeft: [2, 2, 0]}}>
+                                    <Grid item md={6} sx={{p: 2, border: 2, borderColor: 'primary.main'}}>
                                         <Typography variant="h6" component="h4" gutterBottom>Udviklingsværktøjer</Typography>
                                         <Typography variant="body2">En stor del af det vi arbejdede med hos Admill var været med <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>ASP.NET MVC</Typography>, både indenfor at <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>Performance Optimize</Typography>, tilføjelse af <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>ekstra funktionaliteter</Typography> indenfor <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>SEO</Typography> og <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>Content writing/creation</Typography>. Dertil har jeg også arbejdet med <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>React</Typography> og styring af states til <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>multistep forms</Typography>, og til <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>administrationspaneler</Typography>.</Typography>
                                     </Grid>
                                 </Grid>
                             </TabPanel>
-                            <TabPanel value="1" sx={{padding: 0}}>
+                            <TabPanel value="1" sx={{padding: 0, flexGrow: 1}}>
                                 <Grid container spacing={0}>
                                     <Grid item md={6} sx={{bgcolor: 'secondary.dark', p:2}}>
                                         <Typography variant="h6" component="h4">Skoleprojekt (UI/UX)</Typography>
                                         <Typography variant="subtitle1" component="h4">Novicell.dk</Typography>
                                         <BodyText>Som en del af mit skoleprojekt som webudvikler. Fik jeg til opgave at skulle redesign deres side.</BodyText>
                                     </Grid>
-                                    <Grid item md={6} sx={{p: 2, borderTop: 2, borderRight: 2, borderBottom: 2, borderColor: 'primary.main', borderLeft: [2, 2, 0]}}>
+                                    <Grid item md={6} sx={{p: 2, border: 2, borderColor: 'primary.main'}}>
                                         <Typography variant="h6" component="h4" gutterBottom>Udviklingsværktøjer</Typography>
                                         <Typography variant="body2">En stor del af det forløb bestod i <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>UI/UX</Typography>, med både at komme op med nogle nye <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>komponenter</Typography>. Dertil fik jeg også arbejdet med design programmet <Typography variant="body2" component="span" sx={{fontWeight: 'bold', color: 'primary.main'}}>Adobe XD</Typography>.</Typography>
                                     </Grid>
