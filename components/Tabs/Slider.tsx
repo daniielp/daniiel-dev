c7c6a68bc84a579bd561d4cbe23f499919a773cc
shorcy <59348193+shorcy@users.noreply.github.com>
2022-03-10 13:01:18 +0100

    added animation to hero

--- components/Tabs/Slider.tsx
+++ components/Tabs/Slider.tsx
@@ -1,10 +1,14 @@
-import { Container, Grid, Tab, Tabs, Typography, Hidden } from '@mui/material';
+import { Container, Grid, Tab, Tabs, Typography, Hidden, createSvgIcon } from '@mui/material';
 import { Theme } from '@mui/material/styles'
 import TextBundle from '../Typography/TextBundle'
 import React, { useState } from 'react'
 import { Box } from '@mui/material'
-import { Breadcrumbs } from '@mui/material'
+import { Breadcrumbs, SvgIcon } from '@mui/material'
 import { TabContext, TabPanel } from '@mui/lab';
+import Image from 'next/image';
+import DenmarkVector from '../../assets/images/denmark-vector.svg';
+import { keyframes } from '@emotion/react'
+
 
 
 const Slider = () => {
@@ -14,11 +18,24 @@ const Slider = () => {
         setIndex(newIndex);
     }
 
+    const bounce = keyframes`
+    from {
+        opacity: 1;
+        transform: scale3d(1,1,1);
+    }
+
+    to {
+        opacity: 0;
+        transform: scale3d(2,2,2);
+    }
+    `;
+
+
     return (
-        <Box sx={{py: [5, 5, 10], bgcolor: 'secondary.dark' }} component="section">
+        <Box sx={{ py: [5, 5, 10], bgcolor: 'secondary.dark' }} component="section">
             <Container maxWidth="lg">
                 <TabContext value={index.toString()}>
-                    <TabPanel value={"0"} sx={{p: 0}}>
+                    <TabPanel value={"0"} sx={{ p: 0 }}>
                         <Grid container spacing={5}>
                             <Grid item md={6}>
                                 <TextBundle title="UI/UX Designer og Developer, baseret i Aarhus." subtitle="Introduktion" text="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." />
@@ -26,12 +43,13 @@ const Slider = () => {
                                     <Button variant="text" color="primary" endIcon={<ArrowForwardIosIcon />}>Min historie</Button>
                                 </Link> */}
                             </Grid>
-                            <Grid item md={6}>
-                                <Typography variant="h1" component="h1">Daniel <Typography variant="h1" component="span" color="primary">Pedersen</Typography> </Typography>
+                            <Grid item md={6} sx={{ position: "relative", display: "flex" }}>
+                                <Box sx={{ position: "absolute", width: 10, height: 10, zIndex: 1, opacity: 0, background: "yellow", top: "51.4%", left: "34.4%", borderRadius: "50%", animation: `${bounce} 1.5s infinite`}}></Box>
+                                <Image src={DenmarkVector} alt="vector map" width="556" height="600" layout="fixed" />
                             </Grid>
                         </Grid>
                     </TabPanel>
-                    <TabPanel value={"1"} sx={{p: 0}}>
+                    <TabPanel value={"1"} sx={{ p: 0 }}>
                         <Grid container spacing={5}>
                             <Grid item md={6}>
                                 <TextBundle title="Hedder du rigtigt Daniiel med dobbelt i?" subtitle="Historien bag" text="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." />
@@ -41,7 +59,7 @@ const Slider = () => {
                             </Grid>
                         </Grid>
                     </TabPanel>
-                    <TabPanel value={"2"} sx={{p: 0}}>
+                    <TabPanel value={"2"} sx={{ p: 0 }}>
                         <Grid container spacing={5}>
                             <Grid item md={6}>
                                 <TextBundle title="Du er altid velkommen til at kontakte mig" subtitle="Kontakt" text="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." />
@@ -52,17 +70,17 @@ const Slider = () => {
                         </Grid>
                     </TabPanel>
                 </TabContext>
-                <Box display="flex" sx={{display: 'flex', pt: 5}}>
+                <Box display="flex" sx={{ display: 'flex', pt: 5 }}>
                     <Hidden mdDown>
-                        <Breadcrumbs sx={{color: 'text.primary', typography: 'h4'}}>
-                            <Typography variant="h4" component="p" sx={{color: 'primary.main'}}>{index + 1}</Typography>
+                        <Breadcrumbs sx={{ color: 'text.primary', typography: 'h4' }}>
+                            <Typography variant="h4" component="p" sx={{ color: 'primary.main' }}>{index + 1}</Typography>
                             <Typography variant="h4" component="p">3</Typography>
                         </Breadcrumbs>
                     </Hidden>
-                    <Tabs value={index} onChange={handleChange} TabIndicatorProps={{style: {bottom: 'unset', top: 0, height: 10}}} indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile>
-                        <Tab label="Introduktion" sx={{borderTop: 10, borderColor: (theme : Theme) => theme.palette.grey[500], mx: 2}}></Tab>
-                        <Tab label="Historien bag" sx={{borderTop: 10, borderColor: (theme : Theme) => theme.palette.grey[500], mx: 2}}></Tab>
-                        <Tab label="Kontakt" sx={{borderTop: 10, borderColor: (theme : Theme) => theme.palette.grey[500], mx: 2}}></Tab>
+                    <Tabs value={index} onChange={handleChange} TabIndicatorProps={{ style: { bottom: 'unset', top: 0, height: 10 } }} indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto" allowScrollButtonsMobile>
+                        <Tab label="Introduktion" sx={{ borderTop: 10, borderColor: (theme: Theme) => theme.palette.grey[500], mx: 2 }}></Tab>
+                        <Tab label="Historien bag" sx={{ borderTop: 10, borderColor: (theme: Theme) => theme.palette.grey[500], mx: 2 }}></Tab>
+                        <Tab label="Kontakt" sx={{ borderTop: 10, borderColor: (theme: Theme) => theme.palette.grey[500], mx: 2 }}></Tab>
                     </Tabs>
                 </Box>
             </Container>
