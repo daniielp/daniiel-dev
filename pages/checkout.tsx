c27076e500f018a7afe56c9206fab2ba086bed52
shorcy <59348193+shorcy@users.noreply.github.com>
2022-04-07 12:57:54 +0200

    Update checkout.tsx

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -13,39 +13,23 @@ declare global {
 function Checkout() {
     const router = useRouter();
     const { paymentId } = router.query;
-    // useEffect(() => {
-    //     const { paymentId } = router.query;
-    //     const checkoutOptions = {
-    //         checkoutKey: process.env.NEXT_PUBLIC_CHECKOUT_KEY,
-    //         paymentId: paymentId,
-    //         containerId: "checkout-container",
-    //     }
-    //     console.log(paymentId)
-    //     const checkout = new window.Dibs.Checkout(checkoutOptions)
-    //     checkout.on('payment-completed', function (response: any) {
-    //         console.log("Payment completed")
-    //     })
-    // }, [])
-
-    useEffect(() => {
-        
-        // const checkoutOptions = {
-        //     checkoutKey: process.env.NEXT_PUBLIC_CHECKOUT_KEY,
-        //     paymentId: paymentId,
-        //     containerId: "checkout-container",
-        // }
-        // const checkout = new window.Dibs.Checkout(checkoutOptions)
-        // checkout.on('payment-completed', function (response: any) {
-        //     console.log("Payment completed")
-        // })
-    }, [paymentId])
 
     return (
         <main>
             <Container sx={{ py: 5, height: '70vh' }}>
                 <Box id="checkout-container"></Box>
             </Container>
-            <Script id="dibs-js" type="text/javascript" src="https://test.checkout.dibspayment.eu/v1/checkout.js?v=1"/>
+            <Script src="https://test.checkout.dibspayment.eu/v1/checkout.js?v=1" strategy="afterInteractive" onLoad={() => {
+                const checkoutOptions = {
+                    checkoutKey: process.env.NEXT_PUBLIC_CHECKOUT_KEY,
+                    paymentId: paymentId,
+                    containerId: "checkout-container",
+                }
+                const checkout = new window.Dibs.Checkout(checkoutOptions)
+                checkout.on('payment-completed', function (response: any) {
+                    console.log("Payment completed")
+                })
+            }} />
         </main>
     )
 
