07daf8e441644d1a4f8bafd602a36cf3f07b139d
shorcy <59348193+shorcy@users.noreply.github.com>
2022-04-12 08:29:33 +0200

    Update cart.tsx

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,4 +1,6 @@
+import { Button, Container, Grid } from '@mui/material';
 import Router from 'next/router'
+import TextBundle from '../components/Typography/TextBundle';
 
 function Cart() {
 
@@ -6,20 +8,32 @@ function Cart() {
         const response = await fetch("api/create-payment");
         const data = await response.json();
 
-        if(!data.paymentId) {
+        if (!data.paymentId) {
             console.error("Error: Please try again.");
             return;
         }
 
         Router.push({
             pathname: "checkout",
-            query: {paymentId: data.paymentId}
+            query: { paymentId: data.paymentId }
         })
-        
+
     }
 
     return (
-        <button onClick={createPayment}>Proceed to checkout</button>
+        <main>
+            <Container maxWidth="lg" sx={{ py: [5, 5, 10] }}>
+                <Grid container spacing={5}>
+                    <Grid item md={6}>
+                        <TextBundle title="Betalingsløsning for webshops" subtitle="Checkout" text="På denne side vil  du kunne prøve kræfter med betalingsløsning målrettet imod webshops. Ved at klikke på knappen nedenfor vil du blive sendt videre til en testside hvor der kan betales med kort og mobilepay." />
+                        <Button variant="contained" sx={{mt: 3}} onClick={createPayment}>Fortsæt til checkout</Button>
+                    </Grid>
+                    <Grid item md={6}>
+                        
+                    </Grid>
+                </Grid>
+            </Container>
+        </main>
     )
 }
 
