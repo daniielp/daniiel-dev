3deae00075446f4900740ca82286d91b967c47fb
shorcy <59348193+shorcy@users.noreply.github.com>
2022-04-28 10:01:44 +0200

    updated packages

--- pages/api/browser/screenshot.ts
+++ pages/api/browser/screenshot.ts
@@ -1,43 +1,5 @@
+import puppeteer from "puppeteer";
 import type { NextApiRequest, NextApiResponse } from "next";
-
-import puppeteer from "puppeteer-core";
-
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-
-async function getOptions() {
-
-  const chrome = (await import("chrome-aws-lambda")) as any;
-
-  let options;
-  if (process.env.NODE_ENV === "development") {
-    options = {
-      args: [],
-      executablePath: exePath,
-      headless: true,
-      defaultViewport: {
-        width: 1920,
-        height: 1080,
-      },
-    };
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-      defaultViewport: {
-        width: 1920,
-        height: 1080,
-      },
-    };
-  }
-  return options;
-}
 
 interface ScreenshotApiRequest extends NextApiRequest {
   body: {
@@ -45,10 +7,7 @@ interface ScreenshotApiRequest extends NextApiRequest {
   };
 }
 
-async function screenshotHandler(
-  req: ScreenshotApiRequest,
-  res: NextApiResponse
-) {
+function screenshotHandler(req: ScreenshotApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const body = req.body;
 
@@ -65,20 +24,25 @@ async function screenshotHandler(
       const domains = requestURL.hostname.split(".");
       const domainName = domains[domains.length - 2];
 
-      const options = await getOptions();
+      puppeteer
+        .launch({
+          defaultViewport: {
+            width: 1920,
+            height: 1080,
+          },
+        })
+        .then(async (browser) => {
+          const page = await browser.newPage();
+          await page.goto(requestURL.toString());
+          await page.waitForNetworkIdle();
+          await page.screenshot({
+            type: "jpeg",
+            quality: 50,
+            path: "public/" + domainName + ".jpg",
+          });
 
-      puppeteer.launch(options).then(async (browser) => {
-        const page = await browser.newPage();
-        await page.goto(requestURL.toString());
-        await page.waitForNetworkIdle();
-        await page.screenshot({
-          type: "jpeg",
-          quality: 50,
-          path: "public/" + domainName + ".jpg",
+          browser.close();
         });
-
-        browser.close();
-      });
 
       return res.status(201).json({
         code: "screenshot_created",
@@ -88,7 +52,7 @@ async function screenshotHandler(
       return res.status(400).json({
         code: "invalid_url",
         message: err.message,
-      });
+      })
     }
   } else {
     return res.status(405).json({
