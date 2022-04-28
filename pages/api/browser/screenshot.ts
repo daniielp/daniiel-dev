9917c4df28d8c7dddefc5722fbd62f29fb98f4be
shorcy <59348193+shorcy@users.noreply.github.com>
2022-04-28 08:45:54 +0200

    Update screenshot.ts

--- pages/api/browser/screenshot.ts
+++ pages/api/browser/screenshot.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import type { NextApiRequest, NextApiResponse } from "next";
+import chrome from "chrome-aws-lambda";
 
 interface ScreenshotApiRequest extends NextApiRequest {
   body: {
@@ -7,7 +8,10 @@ interface ScreenshotApiRequest extends NextApiRequest {
   };
 }
 
-function screenshotHandler(req: ScreenshotApiRequest, res: NextApiResponse) {
+async function screenshotHandler(
+  req: ScreenshotApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     const body = req.body;
 
@@ -25,12 +29,24 @@ function screenshotHandler(req: ScreenshotApiRequest, res: NextApiResponse) {
       const domainName = domains[domains.length - 2];
 
       puppeteer
-        .launch({
-          defaultViewport: {
-            width: 1920,
-            height: 1080,
-          },
-        })
+        .launch(
+          process.env.NODE_ENV === "production"
+            ? {
+                args: chrome.args,
+                executablePath: await chrome.executablePath,
+                headless: chrome.headless,
+                defaultViewport: {
+                  width: 1920,
+                  height: 1080,
+                },
+              }
+            : {
+                defaultViewport: {
+                  width: 1920,
+                  height: 1080,
+                },
+              }
+        )
         .then(async (browser) => {
           const page = await browser.newPage();
           await page.goto(requestURL.toString());
@@ -52,7 +68,7 @@ function screenshotHandler(req: ScreenshotApiRequest, res: NextApiResponse) {
       return res.status(400).json({
         code: "invalid_url",
         message: err.message,
-      })
+      });
     }
   } else {
     return res.status(405).json({
