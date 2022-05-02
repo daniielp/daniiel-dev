ed45ad38b1d94c7449d3814a71f4fe23bd01fcce
shorcy <59348193+shorcy@users.noreply.github.com>
2022-05-02 11:52:37 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -84,7 +84,7 @@ const Hero = () => {
                 <Grid container spacing={5}>
                     <Grid item md={6}>
                         <TextBundle title="Udvikler med høje ambitioner" subtitle="Om mig" text="Min vision er at hjælpe små og mellemstore virksomheder med at få lavet en skræddersyet hjemmeside, som passer til deres online tilstedeværelse, på det vi alle kender som internettet. Ved at vælge mig er du sikret en skræddersyet hjemmeside kodet fra bunden." />
-                        <Link href="/omkring" underline="hover">
+                        <Link href="#historien" underline="hover">
                             Min historie
                         </Link>
                     </Grid>
