ce4cdf38767b0dead4db421728e3bbdf379454e6
shorcy <59348193+shorcy@users.noreply.github.com>
2022-05-09 10:48:40 +0200

    update heading

--- components/Tabs/Slider.tsx
+++ components/Tabs/Slider.tsx
@@ -39,7 +39,7 @@ const Slider = () => {
                     <TabPanel value={"0"} sx={{ p: 0 }}>
                         <Grid container spacing={5}>
                             <Grid item md={6}>
-                                <TextBundle title="UI/UX Designer og Developer, baseret i Aarhus." subtitle="Introduktion" text="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." />
+                                <TextBundle title="UI/UX Designer og Developer, baseret i Aarhus." subtitle="Introduktion" text="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." primaryHeading={true} />
                                 {/* <Link href="/omkring">
                                     <Button variant="text" color="primary" endIcon={<ArrowForwardIosIcon />}>Min historie</Button>
                                 </Link> */}
--- components/Typography/TextBundle.tsx
+++ components/Typography/TextBundle.tsx
@@ -5,13 +5,14 @@ interface textBunde {
     title: string,
     subtitle: string,
     text: string,
+    primaryHeading: boolean
 }
 
-const TextBundle = ({title, subtitle, text}: textBunde) => {
+const TextBundle = ({title, subtitle, text, primaryHeading = false}: textBunde) => {
     return (
         <>
             <Typography variant="h3" component="h3" color="textSecondary" gutterBottom>- {subtitle}</Typography>
-            <Typography variant="h2" component="h2" gutterBottom>{title}</Typography>
+            <Typography variant="h2" component={primaryHeading ? "h1" : "h2"} gutterBottom>{title}</Typography>
             <BodyText>{text}</BodyText>
         </>
     )
