4d6c8b16240b8cb84fd3896a32356c22c346eb48
shorcy <59348193+shorcy@users.noreply.github.com>
2022-05-17 14:10:33 +0200

    Update Open Graph

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,8 @@ export default class MyDocument extends Document {
                     <meta property="og:image:type" content="image/jpg" />
                     <meta property="og:image:width" content="300" />
                     <meta property="og:image:height" content="300" />
+                    <meta property='og:title' content="UI/UX Designer og Developer, baseret i Aarhus." />
+                    <meta property='og:description' content="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads." />
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
