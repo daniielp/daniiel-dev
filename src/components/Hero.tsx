e3a5628d471c4aa76c24c5b79a05774d59fc92af
danielP <59348193+shorcy@users.noreply.github.com>
2023-01-19 11:52:41 +0100

    Update hero link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,9 +14,9 @@ const Hero = () => {
                   className="flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
                 >
                   <span className="rounded-full bg-indigo-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
-                    We're hiring
+                    Daniiel Pedersen
                   </span>
-                  <span className="ml-4 text-sm">Visit our careers page</span>
+                  <span className="ml-4 text-sm">Læs mere omkring mig</span>
                   <ChevronRightIcon className="ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
                 </a>
               </div>
