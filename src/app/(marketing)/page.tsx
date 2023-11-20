ea90682e3feec0922e34ac663c09f9607e43c331
danielP <daniel.horsens@gmail.com>
2023-11-20 22:48:37 +0100

    Updated header

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,24 +1,28 @@
 import About from "@/components/About";
 import Daniiel from "@/components/Daniiel";
-// import Hero from "@/components/Hero";
-import Hero from "@/components/elements/hero"
-import Grid, { GridItem } from "@/components/Grid";
-// import HeroGlobe from "@/components/elements/hero/HeroGlobe";
 import Projects from "@/components/Projects";
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Link from "next/link";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 export default async function IndexPage() {
     return (
         <>
-
-            <Hero.HeroGlobe
+            <Hero
                 heading={<><span className="block">Frontend udvikler </span><span className="block text-indigo-400">baseret i Århus</span></>}
                 description="Med en kombination af uddannelsen som webudvikler, og med et skarpt øje for design og UI/UX, skaber vækst og gør mig i stand til at arbejde helhjertet med frontend-development. På den måde styrker jeg brandets position med at være de bedste når det kommer til frontend-development, og samtidig skaber værdifulde resultater, som i sidste ende giver bedre købeklare leads."
-                cta={{
-                    heading: "Daniiel Pedersen",
-                    description: "Læs mere omkring mig",
-                    url: "/#about"
-                }}
+                cta={<div className="hidden sm:mb-5 sm:flex sm:justify-center lg:justify-start">
+                    <Link
+                        href="/#about"
+                        className="flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+                    >
+                        <span className="rounded-full bg-indigo-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+                            Daniiel Pedersen
+                        </span>
+                        <span className="ml-4 text-sm">Læs mere omkring mig</span>
+                        <ChevronRightIcon className="ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
+                    </Link>
+                </div>}
             />
             <Projects />
             <About />
---
+++ src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import { classNames } from "@/lib/utils"
+import Image from "next/image"
+import React, { type ReactNode } from "react"
+
+interface HeroProps extends React.HTMLAttributes<HTMLDivElement> {
+    heading: ReactNode,
+    description: ReactNode,
+    cta: ReactNode
+}
+
+const Hero = React.forwardRef<HTMLDivElement, HeroProps>(({ heading, description, cta, className, ...props }, ref) => {
+    return (
+        <div ref={ref} {...props} className={classNames("bg-gray-900 pt-10 sm:pt-16 pb-10 sm:pb-16 lg:overflow-hidden lg:pt-8 lg:pb-14", className)}>
+            <div className="mx-auto max-w-7xl lg:px-8">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                    <div className="mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+                        <div className="lg:py-24">
+                            {cta}
+                            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl lg:mt-6 xl:text-6xl">
+                                {heading}
+                            </h1>
+                            <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                                {description}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="-mb-16 sm:-mb-48 lg:relative lg:m-0">
+                        <Image width={600} height={600} src="/images/denmark-animation.svg" alt="Ping animation for aarhus, Denmark" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+})
+
+
+export default Hero
