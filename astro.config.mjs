96eaf0493e0e45305711f0e288a0f5d3bca472a0
Daniel Pedersen <59348193+daniielp@users.noreply.github.com>
2024-07-24 13:51:25 +0200

    Added shadcn + example to blogpost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://daniiel.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react(), mdx()]
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function formatDate(date: Date | string) {
   const formatedDate = new Date(date);
   return new Intl.DateTimeFormat("da-DK", { dateStyle: "long" }).format(
