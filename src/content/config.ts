cbc8a557f31acba40a083bfb2d2495b44e5991b0
Daniel Pedersen <59348193+daniielp@users.noreply.github.com>
2024-10-13 12:00:49 +0200

    Update to astro 5 Beta

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,9 @@
 // 1. Import utilities from `astro:content`
+import { glob } from "astro/loaders"
 import { z, defineCollection } from "astro:content"
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/blog" }),
   schema: z.object({
     isDraft: z.boolean().default(true),
     title: z.string(),
@@ -19,7 +20,7 @@ const blogCollection = defineCollection({
 })
 
 const policyCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/policy" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -28,7 +29,7 @@ const policyCollection = defineCollection({
 })
 
 const caseCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/case" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
